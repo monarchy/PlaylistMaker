8fe92d7d18255bb7898463a090e4fc4689f6a59e
Nikita Novikov <ebalomeshatel@yandex.ru>
2025-02-17 12:21:35 +0300

    init

--- app/src/main/java/com/example/playlistmaker/Track.kt
+++ app/src/main/java/com/example/playlistmaker/Track.kt
@@ -1,3 +1,6 @@
 package com.example.playlistmaker
 
-data class Track(val trackId: Long, val trackName: String, val artistName: String, val trackTime: Long, val artworkUrl100: String?)
+data class Track(val trackId: Long, val trackName: String,
+                 val artistName: String, val trackTime: Long, val artworkUrl100: String?,
+                 val collectionName: String?, val releaseDate: String?,
+                 val primaryGenreName: String?, val country: String?)
