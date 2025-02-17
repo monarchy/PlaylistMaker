7230ce91dec4bd21c8d8b480ed364bc732e79fad
Nikita Novikov <ebalomeshatel@yandex.ru>
2025-02-17 14:10:28 +0300

    2

---
+++ app/src/main/java/com/example/playlistmaker/Constants.kt
@@ -0,0 +1,13 @@
+package com.example.playlistmaker
+
+object Constants {
+    const val TRACK_ID = "TRACK_ID"
+    const val TRACK_NAME = "TRACK_NAME"
+    const val ARTIST_NAME = "ARTIST_NAME"
+    const val COLLECTION_NAME = "COLLECTION_NAME"
+    const val RELEASE_DATE = "RELEASE_DATE"
+    const val PRIMARY_GENRE_NAME = "PRIMARY_GENRE_NAME"
+    const val COUNTRY = "COUNTRY"
+    const val TRACK_TIME_MILLIS = "TRACK_TIME_MILLIS"
+    const val ART_WORK_URL = "ART_WORK_URL"
+}
--- app/src/main/java/com/example/playlistmaker/Track.kt
+++ app/src/main/java/com/example/playlistmaker/Track.kt
@@ -1,6 +1,9 @@
 package com.example.playlistmaker
 
-data class Track(val trackId: Long, val trackName: String,
+data class Track(val trackId: Int, val trackName: String,
                  val artistName: String, val trackTime: Long, val artworkUrl100: String?,
-                 val collectionName: String?, val releaseDate: String?,
-                 val primaryGenreName: String?, val country: String?)
+                 val collectionName: String, val releaseDate: String,
+                 val primaryGenreName: String, val country: String)
+{
+    fun getCoverArtwork(): String? = artworkUrl100?.replaceAfterLast('/', "512x512bb.jpg")
+}
