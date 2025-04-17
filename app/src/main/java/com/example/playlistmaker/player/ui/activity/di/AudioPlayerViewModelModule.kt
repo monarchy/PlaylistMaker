69beafca366162c8ef7c3aff25d369476903dd01
Nikita Novikov <ebalomeshatel@yandex.ru>
2025-04-17 13:00:09 +0300

    fixes

--- app/src/main/java/com/example/playlistmaker/player/ui/activity/di/AudioPlayerViewModelModule.kt
+++ app/src/main/java/com/example/playlistmaker/player/ui/activity/di/AudioPlayerViewModelModule.kt
@@ -1,11 +1,11 @@
 package com.example.playlistmaker.player.ui.activity.di
 
+import android.media.MediaPlayer
 import com.example.playlistmaker.player.ui.viewmodel.AudioPlayerViewModel
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
 val playerViewModelModule = module {
-    viewModel {
-        AudioPlayerViewModel()
-    }
+    factory { MediaPlayer() }
+    viewModel { AudioPlayerViewModel(mediaPlayer = get()) }
 }
