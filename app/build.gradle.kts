973244326d2634619fe9bb93cfdafccae626f45a
Nikita Novikov <ebalomeshatel@yandex.ru>
2025-06-24 12:29:33 +0300

    тест

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -31,21 +31,26 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
     }
     kotlinOptions {
-        jvmTarget = "1.8"
+        jvmTarget = "11"
     }
+    packaging {
+        resources {
+            excludes += "/META-INF/LICENSE.md"
+            excludes += "/META-INF/LICENSE-notice.md"
+        }
 }
 
 dependencies {
-    implementation (libs.glide)
+    implementation(libs.glide)
     implementation(libs.retrofit)
     implementation(libs.converter.gson)
     implementation(libs.androidx.lifecycle.common.jvm)
     implementation(libs.androidx.navigation.fragment)
-    annotationProcessor (libs.compiler)
+    annotationProcessor(libs.compiler)
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
@@ -54,11 +59,15 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
-    implementation ("androidx.core:core-ktx:1.9.0")
-    implementation ("androidx.viewpager2:viewpager2:1.0.0")
+    implementation("androidx.core:core-ktx:1.9.0")
+    implementation("androidx.viewpager2:viewpager2:1.0.0")
     implementation("io.insert-koin:koin-android:3.3.0")
-    implementation ("androidx.fragment:fragment-ktx:1.8.6")
-    implementation ("androidx.navigation:navigation-ui-ktx:2.8.9")
+    implementation("androidx.fragment:fragment-ktx:1.8.6")
+    implementation("androidx.navigation:navigation-ui-ktx:2.8.9")
+    androidTestImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.8.0")
+    androidTestImplementation ("io.mockk:mockk-android:1.13.8")
+    androidTestImplementation("androidx.arch.core:core-testing:2.2.0")
+    testImplementation ("io.mockk:mockk:1.13.8")
 
-
+}
 }
